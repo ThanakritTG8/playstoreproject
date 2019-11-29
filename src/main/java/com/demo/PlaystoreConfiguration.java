3934eb011c50574fc99dc018dc687793f3e938fe
ThanakritTG8 <tgrock1997@gmail.com>
2019-11-29 16:44:34 +0700

    oh yeah

--- src/main/java/com/demo/PlaystoreConfiguration.java
+++ src/main/java/com/demo/PlaystoreConfiguration.java
@@ -4,17 +4,14 @@ package com.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-@EnableWebSecurity
-public class PlaystoreConfiguration extends WebSecurityConfigurerAdapter{
+
+public class PlaystoreConfiguration{
 //    @Override
 //    proteced void configure(HttpSecurity http) throws Exception{
 //        http
@@ -24,12 +21,7 @@ public class PlaystoreConfiguration extends WebSecurityConfigurerAdapter{
 //        .and().httpBasic();
 //    }
 //    
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
-        auth.inMemoryAuthentication().withUser("myuser").password("password").roles("USER");
-    }
-            
-    @SuppressWarnings("deprecation")      
+                  
     @Bean
     public WebMvcConfigurer corsConfigurer(){
     return new WebMvcConfigurerAdapter(){
